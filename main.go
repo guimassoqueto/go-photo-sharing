eb3026a7108561907efa3a8fb4a9347a8fe72dad
guimassoqueto <gui.massoqueto@gmail.com>
2023-12-10 15:15:20 -0300

    chore: l11

--- main.go
+++ main.go
@@ -6,11 +6,15 @@ import (
 )
 
 func handleFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1><Welcome to my awesome website!</h1>")
+	message := "<h1>Welcome to my awesome website!</h1>"
+	fmt.Fprint(w, message)
 }
 
 func main() {
 	http.HandleFunc("/", handleFunc)
 	fmt.Println("Starting server on :3000...")
-	http.ListenAndServe(":3000", nil)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		panic(err)
+	}
 }
