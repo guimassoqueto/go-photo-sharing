303ac3a430ed3be8851e7c57ef4e5aaad73d333c
guimassoqueto <gui.massoqueto@gmail.com>
2023-12-16 15:36:24 -0300

    chore(L18): basic router

--- main.go
+++ main.go
@@ -5,16 +5,44 @@ import (
 	"net/http"
 )
 
-func handleFunc(w http.ResponseWriter, r *http.Request) {
+func homeHandler(w http.ResponseWriter, r *http.Request) {
 	message := "<h1>Welcome to my awesome website!</h1>"
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprint(w, message)
 }
 
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	message := "<h1>Contact Page</h1>"
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	fmt.Fprint(w, message)
+}
+
+func notFoundHandler(w http.ResponseWriter, r *http.Request) {
+	message := "<h1>Page not found</h1>"
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, message)
+}
+
+type Router struct {}
+
+func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		homeHandler(w, r)
+	case "/contact":
+		contactHandler(w, r)
+	default:
+		notFoundHandler(w, r)
+	}
+}
+
+
 func main() {
-	http.HandleFunc("/", handleFunc)
-	fmt.Println("Starting server on :3000...")
-	err := http.ListenAndServe(":3000", nil)
+	var router Router
+	err := http.ListenAndServe(":3000", router)
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("Starting server on :3000...")
 }
