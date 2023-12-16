8397d58520ed9f53309ba130e127896bd1a9674f
guimassoqueto <gui.massoqueto@gmail.com>
2023-12-16 17:17:53 -0300

    chore(L20): exercise (add a FAQ page)

--- main.go
+++ main.go
@@ -24,6 +24,20 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, message)
 }
 
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	message := `
+<h1>FAQ</h1>
+<h2>Is there a free version?</h2>
+<p>Yes, now go fuck yourself slowly.</h2>
+<h2>How do I pay?</h2>
+<p>With money, bitch.</h2>
+`
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, message)
+}
+
+
 type Router struct {}
 
 func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -32,6 +46,8 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		homeHandler(w, r)
 	case "/contact":
 		contactHandler(w, r)
+	case "/faq":
+		faqHandler(w, r)
 	default:
 		notFoundHandler(w, r)
 	}
@@ -40,9 +56,9 @@ func (router Router) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	var router Router
+	fmt.Println("Starting server on :3000...")
 	err := http.ListenAndServe(":3000", router)
 	if err != nil {
 		panic(err)
 	}
-	fmt.Println("Starting server on :3000...")
 }
