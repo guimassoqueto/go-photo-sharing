39a38a620e197c75bb7ba9e2a74e6adad90b636d
guimassoqueto <gui.massoqueto@gmail.com>
2023-12-17 10:11:07 -0300

    refactor: update templates to use tamplate function helper

--- main.go
+++ main.go
@@ -10,26 +10,37 @@ import (
 	"github.com/go-chi/chi/v5"
 )
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	tmplPath := filepath.Join("templates", "home.gohtml") //TODO: change to not hardcode the path
-	tmpl, err := template.ParseFiles(tmplPath)
-    if err != nil {
+func executeTemplate(w http.ResponseWriter, tmplPath string, data interface{}) {
+	path := filepath.Join(tmplPath)
+	tmpl, err := template.ParseFiles(path)
+	if err != nil {
 		log.Printf("parsing template failed: %v", err)
-        http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
-        return
-    }
-    w.Header().Set("Content-Type", "text/html; charset=utf-8")
-    err = tmpl.Execute(w, struct{Name string}{Name: "John"})
-    if err != nil {
+		http.Error(w, "There was an error parsing the template", http.StatusInternalServerError)
+		return
+	}
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	err = tmpl.Execute(w, data)
+	if err != nil {
 		log.Printf("executing template failed: %v", err)
-        http.Error(w, "There was an error executing the temmplate", http.StatusInternalServerError)
-    }
+		http.Error(w, "There was an error executing the temmplate", http.StatusInternalServerError)
+	}
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	executeTemplate(w, filepath.Join("templates", "home.gohtml"), nil)
 }
 
 func contactHandler(w http.ResponseWriter, r *http.Request) {
-	message := "<h1>Contact Page</h1>"
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	fmt.Fprint(w, message)
+	executeTemplate(w, filepath.Join("templates", "contact.gohtml"), nil)
+}
+
+func faqHandler(w http.ResponseWriter, r *http.Request) {
+	data := []struct{ Question string; Answer string }{
+		{"What is your name?", "My name is Go."},
+		{"What is your quest?", "To learn Go."},
+		{"What is your favorite color?", "Blue."},
+	}
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"), data)
 }
 
 func notFoundHandler(w http.ResponseWriter, r *http.Request) {
@@ -38,20 +49,6 @@ func notFoundHandler(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusNotFound)
 	fmt.Fprint(w, message)
 }
-
-func faqHandler(w http.ResponseWriter, r *http.Request) {
-	message := `
-<h1>FAQ</h1>
-<h2>Is there a free version?</h2>
-<p>Yes, now go fuck yourself slowly.</h2>
-<h2>How do I pay?</h2>
-<p>With money, bitch.</h2>
-`
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, message)
-}
-
 
 func main() {
 	r := chi.NewRouter()
