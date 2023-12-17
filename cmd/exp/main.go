4cf1a2fff5876d2d289f696c5e4ca83bfc99fc7d
guimassoqueto <gui.massoqueto@gmail.com>
2023-12-17 06:28:01 -0300

    chore: basic templating (stdout)

---
+++ cmd/exp/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"html/template"
+	"os"
+)
+
+type User struct {
+	Name string
+	Age int
+	Meta UserMeta
+}
+
+type UserMeta struct {
+	Visits int
+}
+
+func main() {
+	t, err := template.ParseFiles("hello.gohtml")
+	if err != nil {
+		panic(err)
+	}
+	user := User{
+		Name: "Jordi Polla",
+		Age: 29,
+		Meta: UserMeta{
+			Visits: 12,
+		},
+	}
+	err = t.Execute(os.Stdout, user)
+	if err != nil {
+		panic(err)
+	}
+}
