9b67b9f358b5d9cd50b504b45581d3d15fb788bf
guimassoqueto <gui.massoqueto@gmail.com>
2024-01-16 05:26:01 -0300

    chore (L86): playing with go postgres queries

--- cmd/pgx/main.go
+++ cmd/pgx/main.go
@@ -15,9 +15,34 @@ type PostgresConfig struct {
 	sslmode  string
 }
 
+// String returns a string representation of the PostgresConfig struct.
+// The string is formatted as a PostgreSQL connection string.
+//
+// host: The host of the PostgreSQL server.
+// port: The port of the PostgreSQL server.
+// user: The user to connect to the PostgreSQL server.
+// password: The password to connect to the PostgreSQL server.
+// dbname: The name of the database to connect to.
+// sslmode: The SSL mode to use when connecting to the server.
+//
+// Returns a string that can be used to connect to a PostgreSQL server.
 func (cfg PostgresConfig) String() string {
 	return fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s",
 		cfg.Host, cfg.Port, cfg.User, cfg.Password, cfg.Database, cfg.sslmode)
+}
+
+// Panic checks if the provided error is not nil and panics if it is.
+// err: The error to check. If err is not nil, the function will panic.
+// It's used to halt the execution of the program when a fatal error is encountered.
+func Panic(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+type User struct {
+	ID   int
+	Name string
 }
 
 func main() {
@@ -30,13 +55,48 @@ func main() {
 		sslmode:  	"disable",
 	}
 	db, err := sql.Open("pgx", cfg.String())
-	if err != nil {
-		panic(err)
-	}
+	Panic(err)
 	defer db.Close()
 	err = db.Ping() // test connection
-	if err != nil {
-		panic(err)
-	}
+	Panic(err)
 	fmt.Println("Connected!")
+
+	// create a test table
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS test (
+			id SERIAL PRIMARY KEY, name TEXT
+		);
+	`)
+	Panic(err)
+	fmt.Println("Table created!")
+
+	// name := "Guilherme"
+	// row := db.QueryRow(`INSERT INTO test (name) VALUES ($1) RETURNING *;`, name)
+	// Panic(row.Err())
+	// var user User
+	// err = row.Scan(&user.ID, &user.Name)
+	// Panic(err)
+	// fmt.Println(user.ID, user.Name)
+
+	// id := 2
+	// row := db.QueryRow(`SELECT * FROM test WHERE id = $1;`, id)
+	// Panic(row.Err())
+	// var user User
+	// err = row.Scan(&user.ID, &user.Name)
+	// Panic(err)
+	// fmt.Println(user.ID, user.Name)
+
+	// rows, err := db.Query(`SELECT * FROM test;`)
+	// Panic(err)
+	// var users []User
+	// for rows.Next() {
+	// 	var u User
+	// 	err = rows.Scan(&u.ID, &u.Name)
+	// 	if err != nil {
+	// 		fmt.Println(err)
+	// 		continue
+	// 	}
+	// 	users = append(users, u)
+	// }
+	// fmt.Println(users)
 }
