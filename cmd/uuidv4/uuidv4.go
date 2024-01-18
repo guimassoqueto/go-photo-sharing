da8df597de5a731a64dab381b5f84711fd291634
guimassoqueto <gui.massoqueto@gmail.com>
2024-01-18 20:30:43 -0300

    chore: test uuidv4 function

---
+++ cmd/uuidv4/uuidv4.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"github.com/google/uuid"
+)
+
+// GenerateUUID generates a random UUID and returns it as a string.
+func GenerateUUIDV4() (string, error) {
+    uuid, err := uuid.NewRandom()
+    if err != nil {
+        return "", fmt.Errorf("error generating UUID: %w", err)
+    }
+    return uuid.String(), nil
+}
+
+func main() {
+	uuid, err := GenerateUUIDV4()
+	if err != nil {
+		fmt.Println("Error generating UUID:", err)
+	}
+	fmt.Println(uuid)
+}
