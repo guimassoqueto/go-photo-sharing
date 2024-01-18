cce4e5347ae46bf6b31cf21a623fc17032af280b
guimassoqueto <gui.massoqueto@gmail.com>
2024-01-18 18:57:45 -0300

    feat (L99): add user service

--- models/users.go
+++ models/users.go
@@ -1,7 +1,13 @@
 package models
 
+import "database/sql"
+
 type User struct {
 	ID	   uint64  `json:"id"`
 	Name   string `json:"name"`
 	Email  string `json:"email"`
+}
+
+type UserService struct {
+	DB *sql.DB
 }
